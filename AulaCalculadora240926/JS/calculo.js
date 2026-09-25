daed0214d2d58502d718140a8b31eecd5bbad993
zanonmarcello-art <zanon.marcello@gmail.com>
2026-09-24 22:00:48 -0300

    Add files via upload
    Calcular: Soma, Subtração, Multiplicação, Divisão, Áreas: Triangulo, Quadrado, Triangulo.

---
+++ AulaCalculadora240926/JS/calculo.js
@@ -0,0 +1,26 @@
+var n1 = document.querySelector('#n1');
+var n2 = document.querySelector('#n2');
+var resultado =document.querySelector('#resultado');
+
+function somar() {
+
+resultado.innerHTML = Number(n1.value) + Number(n2.value);
+}
+function subtrair() {
+resultado.innerHTML = Number(n1.value) - Number(n2.value);
+}
+function multiplicar() {
+resultado.innerHTML = Number(n1.value) + Number(n2.value);
+}
+function dividir() {
+resultado.innerHTML = Number(n1.value) + Number(n2.value);
+}
+function areaquadrado() {
+resultado.innerHTML = Number(n1.value) + Number(n2.value);
+}
+function Areatriangulo() {
+resultado.innerHTML = Number(n1.value) + Number(n2.value);
+}
+function arearetangulo() {
+resultado.innerHTML = Number(n1.value) + Number(n2.value);
+}
